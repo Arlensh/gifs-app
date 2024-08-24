d84abf6ffdb71c0bfe15a4528abd84beeb7b01f9
Arlensh <lucianobarrerapaez@gmail.com>
2024-08-24 11:16:22 +0200

    Se sube todo al github para la creación del gifs-app

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,19 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css',
+})
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor() {}
+
+  // searchTag(newTag: string) {
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({ newTag });
+  }
+}
